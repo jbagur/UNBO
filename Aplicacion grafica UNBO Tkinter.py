2913fbf3e590267fac9b60dc229bdb3170b04699
jbagur <jbagur@ufm.edu>
2016-06-27 21:57:01 -0600

    Update Aplicacion grafica UNBO Tkinter.py

--- Aplicacion grafica UNBO Tkinter.py
+++ Aplicacion grafica UNBO Tkinter.py
@@ -1,18 +1,21 @@
+#importamos librerias
 import serial
 from Tkinter import *
 from time import sleep
+#inicia el programa
 print("Sistema iniciado")
+#puerto de onde obtendremos la data
 arduinodata = serial.Serial('COM3',9600)
-
+#funcion que actualiza el form con la informacion mas reciente del puerto
+#toma la data del puerto y esta la añade a una variable
 def update():
     a = 0
     b = 0
     while (1==1):
         myData.set(arduinodata.readline().strip())
-
         root.update()
         sleep(1)
-
+#creacion de nuestra aplicacion grafica
 root=Tk()
 root.title("UNBO")
 root.geometry('350x150+200+200')
@@ -26,6 +29,6 @@ y.pack()
 
 w = Label(root, textvariable = myData)
 w.pack()
-
-root.after(2,update)
+#despues de 1 segundo se actualizara nuestra aplicacion
+root.after(1,update)
 root.mainloop()
