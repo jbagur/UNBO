a8108bca89208ebc9ccb09276ace42c91c7f4a77
jbagur <jbagur@ufm.edu>
2016-06-28 17:48:01 -0600

    Add files via upload

---
+++ RECEIVER.ino
@@ -0,0 +1,23 @@
+
+#include <VirtualWire.h>
+byte message[VW_MAX_MESSAGE_LEN]; // INCOMING BUFFER
+byte messageLength = VW_MAX_MESSAGE_LEN; // INCOMING MESSAGE SIZE
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Device is ready");
+  
+  // INITIALIZE IO AND ISR
+  vw_setup(2000); // BIT PER SECOND
+  vw_rx_start();  // -INITIALIZE RF RECEIVER
+}
+
+void loop() {
+  if (vw_get_message(message, &messageLength)) {
+    Serial.print("Received: ");
+    for (int i = 0; i < messageLength; i++) {
+      Serial.write(message[i]);
+    }
+    Serial.println();
+  }
+}
